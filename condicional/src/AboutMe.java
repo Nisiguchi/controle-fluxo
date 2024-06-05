a918ef7851229dbb976f538349363b6e303a8191
Nicole <nlinonicole@gmail.com>
2024-06-05 17:00:38 -0300

    correção

--- condicional/src/AboutMe.java
+++ condicional/src/AboutMe.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 public class AboutMe {
     public static void main(String[] args) {
         try{
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
 
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
 
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
 
-        System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
+            System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+            scanner.close();
         }
-        catch (InputMismatchException e){
-            System.out.println("Os campos de idade e altura precisam ser númericos");
+            catch (InputMismatchException e){
+                System.err.println("Os campos de idade e altura precisam ser númericos");   
         }
     }
-
 }
